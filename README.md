64b90516672d1a5ae949c7f917b9c4b42cf731ca
Yuiinar <61220394+Yuiinars@users.noreply.github.com>
2022-04-24 11:43:44 +0900

    Sync NoyEDGE Production Environment

--- README.md
+++ README.md
@@ -1,17 +1,4 @@
 # nginx-conf
-Nginx CDN Config 檔
+NoyEDGE for Nginx Config
 
-## 环境声明
-- `/var/nginx` 为您nginx安装目录
-- `domain` 为您配置的域名
-
-## 执行顺序
-1. 执行install.sh
-2. 按conf示例 **自行** 更改配置文件
-3. `nginx -s reload`
-
-
-# 参考文献
-- [SSL Configuration Generator](https://ssl-config.mozilla.org/)
-- [Google](https://www.google.co.jp/)
-- *自己的运维经验*
+目前用于 NoyAcg CDN 生产环境
--- install.sh
+++ install.sh
@@ -1,12 +1,61 @@
 #!/bin/bash
-nginx=`nginx -t 2>&1 | grep configuration`
-if [ ! -z "$nginx"  ];then
-    nginxpath="${nginxtmp%test*}"
-    echo "您的Nginx安装目录:$nginxpath"
-else
-    echo -e "\033[37;31;5m 获取Nginx目录失败,配置结束..\033[39;49;0m"
+
+Nginx_Dir="/etc/nginx"
+
+result=$(id | awk '{print $1}')
+if [[ $result != "uid=0(root)" ]]; then
+    echo "Use Root User, Please."
     exit 1
 fi
-curl https://files.catbox.moe/2iptno.txt > $nginxpath
-echo "安装dhparam完毕 \n"
-echo "OK."
+
+if [[ $(uname -s) != Linux ]]; then
+    echo "Use Linux System, Please."
+    exit 1
+fi
+
+res=`which apt-get 2>/dev/null`
+if [[ "$?" != "0" ]]; then
+    apt update
+    apt upgrade -y
+    apt autoremove -y
+    apt-get install -y apt-transport-https lsb-release ca-certificates socat
+fi
+
+
+# Install Nginx
+curl -sSLo /usr/share/keyrings/deb.sury.org-nginx-mainline.gpg https://packages.sury.org/nginx-mainline/apt.gpg
+curl -sSLo /usr/share/keyrings/deb.sury.org-nginx.gpg https://packages.sury.org/nginx/apt.gpg
+
+sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-nginx-mainline.gpg] https://packages.sury.org/nginx-mainline/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/nginx-mainline.list'
+sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-nginx.gpg] https://packages.sury.org/nginx/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/nginx.list'
+
+apt-get update
+apt-get install nginx-core nginx-common nginx nginx-full -y
+
+if [ ! -d "/etc/nginx/" ]; then
+echo "Error: Nginx Install failed."
+exit 1
+else
+    # Create Nginx Cache Dir
+    mkdir -p /cache/
+    # Create Nginx Logs Dir
+    mkdir -p /etc/nginx/logs/error/
+    mkdir -p /etc/nginx/logs/access/
+    # Creat Nginx Basic Dir
+    mkdir -p /etc/nginx/include/
+    mkdir -p /etc/nginx/access/
+    # Create ACME Cert Dir
+    mkdir -p /var/cert/
+    
+    chmod -R 775 /cache/
+    chmod -R 775 /var/cert/
+
+fi
+
+# Install ACME.SH
+if [ ! -d "~/.acme.sh/" ]; then
+curl  https://get.acme.sh | sh
+fi
+
+# Install dhparam 
+curl -sS https://ssl-config.mozilla.org/ffdhe2048.txt > /path/to/dhparam
